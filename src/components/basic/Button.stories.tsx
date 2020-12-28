81c3749260f4fd1a680ff6e687d5e1a4b0f4e0e8
mandaringit <mandarin.dev@kakao.com>
2020-12-28 10:54:41 +0900

    add: 버튼 스토리 & 디자인

--- src/components/basic/Button.stories.tsx
+++ src/components/basic/Button.stories.tsx
@@ -1,18 +1,85 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 import Button, { CustomButtonProps } from "./Button";
+import styled from "styled-components";
 
 export default {
   title: "Basic/Button",
   component: Button,
-  argTypes: {
-    backgroundColor: { control: "color" },
-  },
+  argTypes: {},
 } as Meta;
 
 const Template: Story<CustomButtonProps> = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  label: "버튼",
+  children: "BUTTON",
+};
+
+export const primaryButton = Template.bind({});
+primaryButton.args = { children: "PRIMARY" };
+export const secondaryButton = Template.bind({});
+secondaryButton.args = { children: "SECONDARY", theme: "secondary" };
+export const tertiaryButton = Template.bind({});
+tertiaryButton.args = { children: "TERTIARY", theme: "tertiary" };
+
+const ButtonWrapper = styled.div`
+  .description {
+    margin-bottom: 0.5rem;
+  }
+  & > div + div {
+    margin-top: 2rem;
+  }
+`;
+
+export const sizes = () => {
+  return (
+    <ButtonWrapper>
+      <div>
+        <div className='description'>small</div>
+        <Button size='small'>BUTTON</Button>
+      </div>
+      <div>
+        <div className='description'>medium</div>
+        <Button size='medium'>BUTTON</Button>
+      </div>
+      <div>
+        <div className='description'>large</div>
+        <Button size='large'>BUTTON</Button>
+      </div>
+    </ButtonWrapper>
+  );
+};
+
+export const disabled = () => {
+  return (
+    <ButtonWrapper>
+      <div>
+        <Button disabled>BUTTON</Button>
+      </div>
+      <div>
+        <Button disabled theme='secondary'>
+          BUTTON
+        </Button>
+      </div>
+      <div>
+        <Button disabled theme='tertiary'>
+          BUTTON
+        </Button>
+      </div>
+    </ButtonWrapper>
+  );
+};
+
+export const customSized = () => {
+  return (
+    <ButtonWrapper>
+      <div>
+        <Button width='20rem'>20rem</Button>
+      </div>
+      <div>
+        <Button width='100%'>100%</Button>
+      </div>
+    </ButtonWrapper>
+  );
 };
--- src/components/basic/Button.tsx
+++ src/components/basic/Button.tsx
@@ -1,42 +1,116 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css, FlattenSimpleInterpolation } from "styled-components";
 
 export interface CustomButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   /**
-   * 버튼 텍스트
+   * 버튼 안의 내용
    */
-  label: string;
+  children: React.ReactNode;
   /**
-   * 버튼 배경색
+   * 클릭 시 호출할 콜백 함수
    */
-  backgroundColor?: string;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  /**
+   * 버튼의 생김새를 설정
+   */
+  theme: "primary" | "secondary" | "tertiary";
+  /**
+   * 버튼의 크기를 설정
+   */
+  size: "small" | "medium" | "large";
+  /**
+   * 버튼을 비활성화
+   */
+  disabled?: boolean;
+  /**
+   * 버튼의 너비를 임의로 설정
+   */
+  width?: string | number;
 }
 
+/**
+ * `Button` 컴포넌트는 어떠한 작업을 트리거할 때 사용합니다.
+ */
+const Button = ({ children, onClick, ...props }: CustomButtonProps) => {
+  return (
+    <StyledButton {...props} onClick={onClick}>
+      {children}
+    </StyledButton>
+  );
+};
+
+Button.defaultProps = {
+  theme: "primary",
+  size: "small",
+};
+
 const StyledButton = styled.button<CustomButtonProps>`
-  background-color: ${(props) => props.backgroundColor || "#35baf6"};
-
+  outline: none;
   border: none;
-  border-radius: 5px;
-  width: 150px;
-  padding: 0.6rem 0.5rem;
+  box-sizing: border-box;
+  height: 2rem;
+  font-size: 0.875rem;
+  border-radius: 0.25rem;
   font-weight: bold;
-  color: white;
-  cursor: pointer;
-  opacity: 0.8;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0rem 1rem;
   transition-duration: 0.3s;
+  width: ${(props) => props.width};
+  ${(props) => themes[props.theme]}
+  ${(props) => size[props.size]}
 
-  :focus {
-    outline: none;
+  &:focus {
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.2);
   }
 
-  :hover {
-    opacity: 1;
+  &:disabled {
+    cursor: not-allowed;
   }
 `;
 
-const Button = (props: CustomButtonProps) => {
-  return <StyledButton {...props}>{props.label}</StyledButton>;
+const themes: { [key: string]: FlattenSimpleInterpolation } = {
+  primary: css`
+    background: #ef6c00;
+    color: white;
+    &:disabled {
+      background: #ffe0b2;
+    }
+  `,
+  secondary: css`
+    background: #e9ecef;
+    color: #343a40;
+    &:disabled {
+      color: #c6d3e1;
+    }
+  `,
+  tertiary: css`
+    background: none;
+    color: #ef6c00;
+    &:disabled {
+      color: #ffe0b2;
+    }
+  `,
+};
+
+const size: { [key: string]: FlattenSimpleInterpolation } = {
+  small: css`
+    height: 1.75rem;
+    font-size: 0.75rem;
+    padding: 0 0.875rem;
+  `,
+  medium: css`
+    height: 2.5rem;
+    font-size: 1rem;
+    padding: 0 1rem;
+  `,
+  large: css`
+    height: 3rem;
+    font-size: 1.125rem;
+    padding: 0 1.5rem;
+  `,
 };
 
 export default Button;
