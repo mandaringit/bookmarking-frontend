74f17d2c375bcb49e10057e3cb71cbff33548e21
mandaringit <mandarin.dev@kakao.com>
2020-12-31 10:51:13 +0900

    refactor: auth & todos API 타입 재구성

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,15 +2,15 @@ import { AxiosResponse } from "axios";
 import { iUser } from "../types/entity";
 import { client } from "./client";
 
-interface iAuthAPI {
-  checkAuth: () => Promise<AxiosResponse<iUser>>;
-  localAuth: (email: string, password: string) => Promise<AxiosResponse<iUser>>;
-}
+type UserResponse = AxiosResponse<iUser>;
 
-const authAPI: iAuthAPI = {
-  checkAuth: async () => await client.get("/auth/checkAuth"),
+const authAPI = {
+  checkAuth: async () => await client.get<any, UserResponse>("/auth/checkAuth"),
   localAuth: async (email: string, password: string) =>
-    await client.post("/auth/local", { email, password }),
+    await client.post<any, UserResponse>("/auth/local", {
+      email,
+      password,
+    }),
 };
 
 export default authAPI;
--- src/api/todos.ts
+++ src/api/todos.ts
@@ -2,22 +2,19 @@ import { AxiosResponse } from "axios";
 import { iTodo } from "../types/entity";
 import { client } from "./client";
 
-interface todosAPI {
-  getTodos: () => Promise<AxiosResponse<iTodo[]>>;
-  addTodo: (text: string) => Promise<AxiosResponse<iTodo>>;
-  toggleTodo: (id: number) => Promise<AxiosResponse<iTodo>>;
-  updateTodoText: (id: number, text: string) => Promise<AxiosResponse<iTodo>>;
-  removeTodo: (id: number) => Promise<AxiosResponse>;
-}
+type TodoResponse = AxiosResponse<iTodo>;
+type TodosResponse = AxiosResponse<iTodo[]>;
 
-const todoAPI: todosAPI = {
-  getTodos: async () => await client.get<iTodo[]>("/todos"),
-  addTodo: async (text: string) => await client.post<iTodo>("/todos", { text }),
+const todoAPI = {
+  getTodos: async () => await client.get<any, TodosResponse>("/todos"),
+  addTodo: async (text: string) =>
+    await client.post<any, TodoResponse>("/todos", { text }),
   toggleTodo: async (todoId: number) =>
-    await client.patch<iTodo>(`/todos/${todoId}/toggle`),
+    await client.patch<any, TodoResponse>(`/todos/${todoId}/toggle`),
   updateTodoText: async (todoId: number, text: string) =>
-    await client.patch<iTodo>(`/todos/${todoId}`, { text }),
-  removeTodo: async (todoId: number) => await client.delete(`/todos/${todoId}`),
+    await client.patch<any, TodoResponse>(`/todos/${todoId}`, { text }),
+  removeTodo: async (todoId: number) =>
+    await client.delete<any, AxiosResponse>(`/todos/${todoId}`),
 };
 
 export default todoAPI;
