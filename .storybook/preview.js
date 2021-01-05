78aae30277b2a05aeaaf973be2b7563c379adbd1
mandaringit <mandarin.dev@kakao.com>
2021-01-05 09:46:30 +0900

    refactor: Storybook Redux Store > Global Decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,17 @@
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../src/pages/auth/authSlice";
+
+const store = configureStore({ reducer: { auth: authReducer } });
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <Provider store={store}>
+      <Story />
+    </Provider>
+  ),
+];
--- src/pages/auth/Login.stories.tsx
+++ src/pages/auth/Login.stories.tsx
@@ -1,15 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
 import { Story, Meta } from "@storybook/react/types-6-0";
-import { Provider } from "react-redux";
 import { PureLogin, PureLoginProps } from "./Login";
-import authReducer from "./authSlice";
-
-const store = configureStore({ reducer: { auth: authReducer } });
 
 export default {
   title: "Pages/Login",
   component: PureLogin,
-  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
 } as Meta;
 
 const Template: Story<PureLoginProps> = (args) => <PureLogin {...args} />;
