76221c2f41bd27c9b01d2fad3cc4f61b1a381ce5
mandaringit <mandarin.dev@kakao.com>
2021-01-06 11:03:27 +0900

    refactor: GlobalStyle 분리 및 포맷팅 안되는 현상 수정

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,15 @@
+import * as styled from "styled-components";
+
+const GlobalStyle = styled.createGlobalStyle`
+  html,
+  body,
+  #root {
+    height: 100%;
+  }
+
+  body {
+    margin: 0;
+  }
+`;
+
+export default GlobalStyle;
--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-import { createGlobalStyle } from "styled-components";
 import { Router } from "react-router-dom";
 import customHistory from "./lib/customHistory";
 import { checkAuth, tempSetUser } from "./slices/authSlice";
-
-const GlobalStyle = createGlobalStyle`
-  html,body,#root {
-    height:100%;
-  }
-  body{
-    margin:0; 
-  }
-`;
+import GlobalStyle from "./GlobalStyle";
 
 if (localStorage.getItem("mandarin-dev")) {
   store.dispatch(
