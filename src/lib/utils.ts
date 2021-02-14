368eb2a5731183bab3d21ed7a65f47c297be7c24
mandaringit <mandarin.dev@kakao.com>
2021-02-14 20:20:29 +0900

    fix: 후방탐색 모바일에서 호환안되는 문제 해결

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,8 +2,10 @@ export const removeHTMLTag = (text: string) =>
   text.replace(/(<([^>]+)>)/gi, "");
 
 export const extractFname = (thumbnail: string) => {
-  const extracted = thumbnail.match(/(?<=(fname=)).+/gi);
-  return extracted ? extracted[0] : "";
+  // 브라우저 후방탐색 지원 오류 있음
+  // const extracted = thumbnail.match(/(?<=(fname=)).+/gi);
+  const extracted = thumbnail.split("fname=");
+  return extracted ? extracted[1] : "";
 };
 
 export const getFullThumbnailUrl = (fname: string, width?: number) => {
